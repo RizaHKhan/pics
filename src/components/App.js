bcca5498d184000d7807669d509e6d0e39ceedec
RizaHKhan <khanriza@gmail.com>
2020-05-18 06:07:15 -0400

    implemented axios and made api call to remote location

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
+import Axios from "axios";
 import SearchBar from "./SearchBar";
 
 export default class App extends Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await Axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID t5pSTfrfLBLouuqocPizOqwE-t-ZC7vGY1UkoY-oca8",
+      },
+    });
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
